ae26260232dd80ae30719862456a07711f4b114b
nurav1997 <nurav1997@users.noreply.github.com>
2020-02-01 12:48:09 +0530

    some changes
    changes

--- login/src/app/user-input/user-input.component.ts
+++ login/src/app/user-input/user-input.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import {User} from '../user'
 @Component({
   selector: 'app-user-input',
   templateUrl: './user-input.component.html',
   styleUrls: ['./user-input.component.css']
 })
 export class UserInputComponent implements OnInit {
-
+  userModel=new User('varun@googl.com','Password@123');
   constructor() { }
 
   ngOnInit() {
