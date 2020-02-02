f189f67d77f793d34a0fbe3dd73420a33da37da5
Sudeep Ratnaparkhe <sudeeppr1998@gmail.com>
2020-02-02 19:49:54 +0530

    Create onSubmit event and validate username and password

--- login/src/app/app-routing.module.ts
+++ login/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Input } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { UserInputComponent } from './user-input/user-input.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [{ path: '', redirectTo: '/userinput', pathMatch: 'full' },
+{path:'userinput',component:UserInputComponent},
+{path:'profile' , component:ProfileComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- login/src/app/user-input/user-input.component.ts
+++ login/src/app/user-input/user-input.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from '../user'
+import {Router} from "@angular/router"
 @Component({
   selector: 'app-user-input',
   templateUrl: './user-input.component.html',
   styleUrls: ['./user-input.component.css']
 })
 export class UserInputComponent implements OnInit {
-  userModel=new User('varun@googl.com','Password@123');
-  constructor() { }
+  userModel=new User('','');
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  onSubmit(){
+   if (this.userModel.email === 'test@domain.com' && this.userModel.password === 'Password123!'){
+      this.router.navigate(["/profile"]);  }
+      else{
+        alert("Invalid Username or Password");
+        this.userModel.email="";
+        this.userModel.password="";
+      }
+  }
 }
