d686053022302589052698efe2f88e115bbe6bb7
nurav1997 <nurav1997@users.noreply.github.com>
2020-02-08 14:11:24 +0530

    validation and routing

--- login/src/app/app-routing.module.ts
+++ login/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
-import { NgModule, Input } from '@angular/core';
+import { NgModule, Input, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
 import { UserInputComponent } from './user-input/user-input.component';
+import { SignupComponent } from './signup/signup.component';
+
 import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [{ path: '', redirectTo: '/userinput', pathMatch: 'full' },
 {path:'userinput',component:UserInputComponent},
-{path:'profile' , component:ProfileComponent}];
+{path:'profile' , component:ProfileComponent},
+{path:'signup',component:SignupComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes),HttpClientModule],
--- login/src/app/signup/signup.component.ts
+++ login/src/app/signup/signup.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {NewUser} from '../new-user'
+import {Router} from "@angular/router"
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -8,10 +10,21 @@ import {NewUser} from '../new-user'
 export class SignupComponent implements OnInit {
    userSignup=new NewUser('','','','','');
    
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
+
+  onSubmit(){
+     
+    if (this.userSignup.email === 'test@domain.com' && this.userSignup.password === 'Password123!'){
+       this.router.navigate(["/profile"]);  }
+       else{
+         alert("SORRY BUT SOME FIELDS ARE INVALID PLEASE CORRECT THEM");
+         this.userSignup.email="";
+         this.userSignup.password="";
+       }
+   } 
 
 }
  
