8e2ce3db2e971e1ba009641bd2ada290a84a3de5
nurav1997 <nurav1997@users.noreply.github.com>
2020-02-09 18:45:29 +0530

    added http post request for login and signup
    created validation for signup page.
    
    added http post request with error catching and console log

--- login/src/app/signup/signup.component.ts
+++ login/src/app/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {NewUser} from '../new-user'
 import {Router} from "@angular/router"
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup',
@@ -10,22 +11,53 @@ import {Router} from "@angular/router"
 export class SignupComponent implements OnInit {
    userSignup=new NewUser('','','','','');
    
-  constructor(private router: Router) { }
+  //constructor(private router: Router) { }
+  constructor(private http: HttpClient){};
 
   ngOnInit() {
   }
 
-  onSubmit(){
+  // onSubmit(){
      
+  //   if (this.userSignup.Password == this.userSignup.cPassword)
+  //   { 
+  //     this.router.navigate(["/userinput"]); 
+  //   }else{      
+  //     alert("SORRY BUT THE PASSWORD AND CONFIRM-PASSWORD DO NOT MATCH :(")
+
+  //    }
+      
+  //  } 
+
+   onSubmit()
+  {
     if (this.userSignup.Password == this.userSignup.cPassword)
     { 
-      this.router.navigate(["/userinput"]); 
-    }else{      
-      alert("SORRY BUT THE PASSWORD AND CONFIRM-PASSWORD DO NOT MATCH :(")
+    this.http.post("https://nalo-test.herokuapp.com/v1/signup" ,
+    {
+      "email":this.userSignup.email,
+      "password":this.userSignup.Password,
+      "name":this.userSignup.userName,
+      "role":this.userSignup.role
+    })
 
-     }
+    .subscribe
+    (
+      data =>{
+        alert("POST REQUEST SENT SUCCESSFULLY");
+        console.log(data);
+      },
+      error =>{
+        alert("ERROR HAS OCCURED");
+        error.log(error);
+      }
       
-   } 
+    )
+  }
+else{ 
+   alert("SORRY PASSWORD AND CONFIRM PASSWORD DO NOT MATCH");
+ }
 
+  }
 }
  
